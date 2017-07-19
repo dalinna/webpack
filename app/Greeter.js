c9bf2e98239ca9a488fa0d0339dd878a72c9fd07
谭琳琳 <805148199@qq.com>
2017-07-19 20:07:12 +0800

    save

---
+++ app/Greeter.js
@@ -0,0 +1,7 @@
+// Greeter.js
+module.exports = function() {
+  var greet = document.createElement('div');
+  greet.textContent = "加油，打包";
+  return greet;
+};
+
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
+  output: {
+    path: __dirname + "/public",//打包后的文件存放的地方
+    filename: "bundle.js"//打包后输出文件的文件名
+  },
+  devServer: {
+    contentBase: './public',//默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在目录（本例设置到"build"目录）
+    inline: true,
+    port: 3333
+  }
+}
